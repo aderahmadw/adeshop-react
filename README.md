ca6b83f69423fd52f4f716d28ffad830000dd394
aderahmadw <aderahmad555@gmail.com>
2023-04-07 13:05:27 +0700

    Lesson 5 creating product list dummy

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 
 3. Create react app
 4. Lesson pushing to github
+5. Create list of products
+    1. create prodicts array
+    2. add its images
+    3. render it
+    4. style it
--- adeshop/frontend/src/App.js
+++ adeshop/frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
@@ -5,7 +7,23 @@ function App() {
         <a href="/">adeshop</a>
       </header>
       <main>
-        list
+        <h1>Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
